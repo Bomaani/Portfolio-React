4f6ac947aa0118e4496ffbd824da9ae92522830d
Brock Boutwell <brockboutwell@gmail.com>
2020-01-11 10:02:54 -0500

    changes to minor css and wording

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,10 +7,7 @@ class About extends Component {
       <div className="AboutContainer">
         <h3>- Brock the Developer -</h3>
         <p>
-          I am currently a Front End Developer and graduate from the Savannah
-          College of Art and Design and Thinkful aspiring to be a Full-Stack Developer. Web Development is my passion becuase it combines my desire to desgin and be artistic with my desire to learn coding and grow with an evolving industry. 
-          I'm a very critical thinker and tackle all of my problems head
-          on and without hesitation. Scroll down to see some of my more recent projects!
+        I am currently a Front End Developer and graduate from the Savannah College of Art and Design and Thinkful aspiring to be a Full-Stack Developer. Web Development is my passion because it combines my desire to design and be artistic with my desire to code, learn, and grow with an evolving industry. I'm a very critical thinker and tackle all of my problems head-on and without hesitation. Scroll down to see some of my more recent projects!
         </p>
       </div>
     );
